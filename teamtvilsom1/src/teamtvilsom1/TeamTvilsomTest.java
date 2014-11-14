24b88323d248e1d71b2fe11fdf65448643ddad6d
Espen Moe <espen357moe@gmail.com>
2014-11-14 20:53:58 +0100

    La inn enda en System.out.println

--- teamtvilsom1/src/teamtvilsom1/TeamTvilsomTest.java
+++ teamtvilsom1/src/teamtvilsom1/TeamTvilsomTest.java
@@ -4,6 +4,7 @@ public class TeamTvilsomTest {
 
 	public static void main(String[] args) {
 		System.out.println("Bravo, det funker..!!");
+		System.out.println("Gidameg, så flinke vi er!");
 	}
 
 }
