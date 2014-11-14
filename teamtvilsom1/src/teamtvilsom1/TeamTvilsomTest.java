e4e88d1d6f81c98132cfd315db21ea9213aa58f4
Jumkah <oysteinpaulsen@hotmail.com>
2014-11-14 13:29:57 +0100

    Rune. Fiks.

--- teamtvilsom1/src/teamtvilsom1/TeamTvilsomTest.java
+++ teamtvilsom1/src/teamtvilsom1/TeamTvilsomTest.java
@@ -3,7 +3,7 @@ package teamtvilsom1;
 public class TeamTvilsomTest {
 
 	public static void main(String[] args) {
-		System.out.println("Feiring er på sin plass når ting fungerer! Men gjør det det da?");
+		System.out.println("Rune ødelegger alt. ALT!");
 	}
 
 }
