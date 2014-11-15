47710a435c25466d53fcf7ab2521b9fa1af2adb1
Espen Moe <espen357moe@gmail.com>
2014-11-15 11:21:46 +0100

    La til enda en linje

--- teamtvilsom1/src/teamtvilsom1/TeamTvilsomTest.java
+++ teamtvilsom1/src/teamtvilsom1/TeamTvilsomTest.java
@@ -5,6 +5,7 @@ public class TeamTvilsomTest {
 	public static void main(String[] args) {
 		System.out.println("Bravo, det funker..!!");
 		System.out.println("Gidameg, så flinke vi er!");
+		System.out.println("*Klapp på skuldra*");
 	}
 
 }
